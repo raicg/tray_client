1f0e178a33ee8017b75f5f12ebabe4c842b9aa98
Lukas Pol <lukaspaespol@gmail.com>
2020-10-14 14:47:21 -0300

    created model products

---
+++ app/models/tray_client/product.rb
@@ -0,0 +1,29 @@
+module TrayClient
+  class Product < OpenStruct
+    class << self
+      def base_url
+        TrayClient::Client.base_url + '/products'
+      end
+
+      def get(token = '', query = {})
+        TrayClient::Client.get(token, base_url, '', {}, '', query)
+      end
+
+      def get_product(token = '', id = '0')
+        TrayClient::Client.get(token, base_url, id, {}, '', query)
+      end
+
+      def create(token = '', query = {})
+        TrayClient::Client.post(token, base_url, '', {}, '', query)
+      end
+
+      def update(token = '', id = '0', query = {})
+        TrayClient::Client.update(token, base_url, id, {}, '', query)
+      end
+
+      def delete(token = '', id = '0')
+        TrayClient::Client.delete(token, base_url, id)
+      end
+    end
+  end
+end

5bb06be73368ec6b767e15f7360e4ef6df945dbd
Lukas Pol <lukaspaespol@gmail.com>
2020-10-22 16:21:35 -0300

    fixing products

--- app/models/tray_client/product.rb
+++ app/models/tray_client/product.rb
@@ -5,25 +5,41 @@ module TrayClient
         TrayClient::Client.base_url + '/products'
       end
 
-      def get(token = '', query = {})
-        TrayClient::Client.get(token, base_url, '', {}, '', query)
+      def get(token)
+        id = ''
+        params = {}
+        endpoint = ''
+        query = { access_token: token }
+        TrayClient::Client.get(base_url, id, params, endpoint, query)
       end
 
-      def get_product(token = '', id = '0')
-        TrayClient::Client.get(token, base_url, id, {}, '', query)
+      def get_details(token, id)
+        endpoint = ''
+        query = { access_token: token }
+        params = {}
+        TrayClient::Client.get(base_url, id.to_s, params, endpoint, query)
       end
 
-      def create(token = '', query = {})
-        TrayClient::Client.post(token, base_url, '', {}, '', query)
+      def create(token, query = {})
+        id = ''
+        params = {}
+        endpoint = ''
+        query[access_token] = token
+        TrayClient::Client.post(base_url, id.to_s, params, endpoint, query)
       end
 
-      def update(token = '', id = '0', query = {})
-        TrayClient::Client.update(token, base_url, id, {}, '', query)
+      def update(token, id, query = {})
+        params = {}
+        endpoint = ''
+        query[access_token] = token
+        TrayClient::Client.update(base_url, id, params, endpoint, query)
       end
 
-      def delete(token = '', id = '0')
-        TrayClient::Client.delete(token, base_url, id)
-      end
+      def delete(token, id)
+        endpoint = ''
+        query = { access_token: token }
+        params = {}
+        TrayClient::Client.delete(base_url, id.to_s, params, endpoint, query)
     end
   end
 end
